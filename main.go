697c500bc0b517e57804b51cc58ece4d11912967
Oscar Bardrick <oscarm-b@tutamail.com>
2020-11-01 19:46:42 +0100

    update for new server

--- main.go
+++ main.go
@@ -2,35 +2,13 @@ package main
 
 import (
 	"flag"
-	"log"
-	"os"
 
-	"github.com/OJOMB/mvsc-microservice/config"
-	"github.com/OJOMB/mvsc-microservice/controllers"
-	"github.com/gorilla/mux"
+	"github.com/OJOMB/mvsc-microservice/server"
 )
 
 var env = flag.String("env", "dev", "The environment in which the server is running ['dev', 'test', 'production']")
 
-func startApp() {
-	flag.Parse()
-
-	// get the logger
-	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
-	logger.Printf("Server is starting...")
-
-	// get the config
-	cnfg := config.ConfigMap[*env]
-
-	//get the router
-	r := mux.NewRouter()
-
-	// Instantiate server with shared dependencies
-	c := controllers.New(r, logger, &cnfg)
-
-	c.ListenAndServe()
-}
-
 func main() {
-	startApp()
+	flag.Parse()
+	server.StartApp(*env)
 }
